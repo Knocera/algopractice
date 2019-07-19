2285e184dc8a83098eecaa998b16c450ccd1ddf6
Keith Nocera <knocera@gmail.com>
2019-07-19 15:31:27 -0400

    solve with optimized solution

--- fewestCoins.js
+++ fewestCoins.js
@@ -1,36 +1,25 @@
 function minNumberOfCoinsForChange(n, denoms) {
   let ways = []
   while (ways.length <= n){
-    ways.push(0)
+    ways.push(Infinity)
   }
-  let divisable = false
-	denoms.sort((a,b)=>a-b)
+  ways[0] = 0
 
 
 
   for (let i = 0; i < denoms.length; i++){
     let coin = denoms[i]
-    if (n % coin === 0){
-      divisable = true
-    }
-    let coinCount = 0
+
     for (let j = 0; j <= n; j++){
 
-      if (j % coin === 0){
-        ways[j] = coinCount
-        coinCount++
-      } else if (coin < j){
-        ways[j] = ways[j - coin] + 1
+      if (coin <= j){
+        ways[j] = Math.min(ways[j - coin] + 1, ways[j]  )
       }
-
-    }
+    console.log(ways)
 
   }
-  if (divisable){
-  console.log(ways[n])
+
+
+  }
   return ways[n]
-  } else {
-    console.log(-1)
-    return -1
-  }
 }
