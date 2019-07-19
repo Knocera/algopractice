32468a78785cbb9043fa1859fecc3f1c0fe147f6
Keith Nocera <knocera@gmail.com>
2019-07-19 13:08:17 -0400

    finished in 1hr in optimal 0(n) time

---
+++ maxSubset.js
@@ -0,0 +1,63 @@
+/* eslint-disable complexity */
+let test1 = [7, 1, 2, 9, 6] // expect (7, 9)
+let test2 = [7, 1, 2, 9, 8] // expect (7, 2, 8)
+let test3 = [7, 1, 2, 9, 6, 3] // expect (7, 9, 3)
+let test4 = [1] // expect 0
+
+
+function maxSubsetSumNoAdjacent(array) {
+  if (array.length < 3){
+    return 'less than 3 nums total: ' + 0
+  }
+  let currentInd = 1
+  let highest = array[0]
+  let highestInd = 0
+  let pH = 0
+  let pHind = 0
+
+  while (currentInd < array.length){
+    let num = array[currentInd]
+    if (num + highest > highest && Math.abs(currentInd - highestInd) !== 1){
+       pH = highest
+       pHind = highestInd
+       highest = num + highest
+       highestInd = currentInd
+    } else if (num + pH > highest && Math.abs(currentInd - pHind) !== 1){
+        let temp = num + pH
+        pH = highest
+        pHind = highestInd
+        highest = temp
+        highestInd = currentInd
+    } else if (highest > num + pH > pH && Math.abs(currentInd - pHind) !== 1){
+      pH = num + pH
+      pHind = currentInd
+    } else if (num > highest){
+          pH = highest
+          pHind = highestInd
+          highest = num
+          highestInd = currentInd
+    } else if (num > pH){
+          pH = num
+          pHind = currentInd
+    }
+    currentInd++
+  }
+return highest
+}
+
+/*
+take current number
+set it as highest
+record index as highest index
+then take next number
+if index of next +1 or -1 of highest then cant add
+
+
+-- go through
+== can we add highest so far to this number
+== can we had previous highest
+== if not can we add original to this number and is it highest
+
+
+-- could think of pushing values into an array
+*/
